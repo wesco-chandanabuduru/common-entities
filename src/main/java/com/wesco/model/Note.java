d074b62709f37d25c1ede55dc0d3c5d362f42c07
wesco-chandanabuduru <102545118+wesco-chandanabuduru@users.noreply.github.com>
2022-07-19 23:03:00 +0530

    Initital commit 3

---
+++ src/main/java/com/wesco/model/Note.java
@@ -0,0 +1,39 @@
+package com.wesco.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
+@Table(name = "notes")
+@EntityListeners(AuditingEntityListener.class)
+@Data
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String content;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
+
+   
+
+}
---
+++ src/main/java/com/wesco/model/Testing.java
@@ -0,0 +1,5 @@
+package com.wesco.model;
+
+public class Testing {
+
+}
